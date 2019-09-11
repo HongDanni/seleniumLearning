3435f8c2e7942aa557f21debbbdb6b11cb8dd229
Danni Hong <dannihong@DannideMacBook-Pro.local>
2019-09-11 21:33:38 +0800

    1

--- 页面等待.py
+++ 页面等待.py
@@ -8,6 +8,20 @@ import random
 driver = webdriver.PhantomJS('/Users/dannihong/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
 driver.set_window_size(1280, 2400)
 
+url = 'https://www.bing.com'
+driver.get(url)
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import selenium.webdriver.support.expected_conditions as ec
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        ec.presence_of_element_located
+    )
+
+
+
 
 
 
